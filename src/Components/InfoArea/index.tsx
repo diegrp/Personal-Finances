0c9fe86c56960a02a0fdc3d9dad37058e8b1bff7
Diego Rocha <dkxever@gmail.com>
2022-02-22 17:28:46 -0300

    Structuring and organizing our InfoArea

--- src/Components/InfoArea/index.tsx
+++ src/Components/InfoArea/index.tsx
@@ -10,6 +10,21 @@ type Props = {
 export const InfoArea = ( { currentMonth, onChangeMonth, income, expense }: Props ) => {
 
   return(
-    <div></div>
+    <C.Container>
+      <C.MonthArea>
+        <C.MonthArrow onClick={handlePrevMonth}>⬅️</C.MonthArrow>
+        <C.MonthTitle>{formatCurrentMonth(currentMonth)}</C.MonthTitle>
+        <C.MonthArrow onClick={handleNextMonth}>➡️</C.MonthArrow>
+      </C.MonthArea>
+      <C.ResumeArea>
+        <ResumeItem title="Receitas" value={income}/>
+        <ResumeItem title="Despesas" value={expense}/>
+        <ResumeItem 
+          title="Balanço" 
+          value={income-expense}
+          color={(income-expense) < 0 ? 'red':'green'}
+        />
+      </C.ResumeArea>
+    </C.Container>
   )
 }
