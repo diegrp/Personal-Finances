3c6f83d925f477402f54491e4c43b55c71448ada
Diego Rocha <dkxever@gmail.com>
2022-02-22 16:58:56 -0300

    Creating currentMonth and filterListByMonth function

---
+++ src/helpers/dateFilter.ts
@@ -0,0 +1,26 @@
+import { Item } from "../types/Item";
+
+// Pega o mês atual
+
+export const getCurrentMonth = () => {
+  let now = new Date();
+  return `${now.getFullYear()} - ${now.getMonth() + 1}`;
+}
+
+// Filtra itens com base no mês
+
+export const filterListByMonth = ( list: Item[], date: string ): Item[] => {
+  
+  let newList: Item[] = [];
+
+  let [ year, month ] = date.split('-');
+  
+  for(let i in list){
+    if(list[i].date.getFullYear() === parseInt(year) && list[i].date.getMonth() + 1 === parseInt(month)){
+      newList.push(list[i]);
+    }
+  }
+
+  return newList;
+
+}
