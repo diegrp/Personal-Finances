5acf8526a5968122e78bec248b4660f729725cdb
Diego Rocha <dkxever@gmail.com>
2022-02-22 17:12:07 -0300

    Structuring and organizing our TableArea

--- src/Components/TableArea/index.tsx
+++ src/Components/TableArea/index.tsx
@@ -1,4 +1,5 @@
 import { Item } from "../../types/Item";
+import { TableItem } from "../TableItem";
 
 export type Props = {
   list: Item[]
@@ -10,9 +11,27 @@ export type Props = {
 export const TableArea = ( { list, handleDeleteItem }: Props ) => {
 
   return(
-    <div>
-
-    </div>
+    <C.Table>
+      <thead>
+        {/* Título dos itens */}
+        <tr>
+          <C.TableHeadColumn width={100}>Data</C.TableHeadColumn>  
+          <C.TableHeadColumn width={100}>Categoria</C.TableHeadColumn>  
+          <C.TableHeadColumn width={630}>Descrição</C.TableHeadColumn>  
+          <C.TableHeadColumn width={100}>Valor</C.TableHeadColumn> 
+        </tr>
+      </thead>
+      <tbody>
+        {/* Map dos nossos itens filtrados */}
+        {list.map((item, index) => (
+          <TableItem 
+            key={index} 
+            item={item}
+            onDelete={handleDeleteItem} 
+          />
+        ))}
+      </tbody>
+    </C.Table>
   )
 
 }
