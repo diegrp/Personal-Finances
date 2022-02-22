d5b039213e13381a4f99693927aa2776929d1011
Diego Rocha <dkxever@gmail.com>
2022-02-22 17:01:50 -0300

    Creating and updating the main states of the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Item } from "./types/Item";
+import { items } from "./data/items";
+import { filterListByMonth, getCurrentMonth } from "./helpers/dateFilter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const [ list, setList ] = useState(items);
+  const [ filteredList, setFilteredList ] = useState<Item[]>([]);
+  const [ currentMonth, setCurrentMonth ] = useState(getCurrentMonth());
+
+  // Filtagem da lista pelo mês atual
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  },[list, currentMonth]);
+
+  return(
+    <div></div>
+  )
 }
 
 export default App;
