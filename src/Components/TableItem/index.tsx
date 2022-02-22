fcd36cc374f5368dc3871cae41b67c313c969dbd
Diego Rocha <dkxever@gmail.com>
2022-02-22 17:22:33 -0300

    Structuring and organizing our TableItem

--- src/Components/TableItem/index.tsx
+++ src/Components/TableItem/index.tsx
@@ -1,3 +1,5 @@
+import { categories } from "../../data/categories";
+import { formatDate } from "../../helpers/dateFilter";
 import { Item } from "../../types/Item";
 
 type Props = {
@@ -9,6 +11,23 @@ type Props = {
 
 export const TableItem = ( { onDelete, item }: Props ) => {
   return(
-    <div></div>
+    <C.TableLine>
+      <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
+      <C.TableColumn>
+        <C.Category color={categories[item.category].color}>
+          {categories[item.category].title}
+        </C.Category>
+      </C.TableColumn>
+      <C.TableColumn>{item.title}</C.TableColumn>
+      <C.TableColumn>
+        <C.Value color={categories[item.category].expense ? 'red':'green'}>
+         R$ {item.value}
+        </C.Value>
+      </C.TableColumn>
+      <C.TableColumn>
+        {/* Botão de delete */}
+        <C.DeleteItem onClick={() => onDelete(item.title)}>X</C.DeleteItem>
+      </C.TableColumn>
+    </C.TableLine>
   )
 }
