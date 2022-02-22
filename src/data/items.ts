d38f4203d0710ee7cdbba4f57f115194dfc46634
Diego Rocha <dkxever@gmail.com>
2022-02-22 16:57:53 -0300

    Creating items and categories with their respective types

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  { date: new Date(2022, 1, 15), category: 'food', title: 'McDonalds', value: 25.30 },
+  { date: new Date(2022, 1, 22), category: 'food', title: 'Burger King', value: 15.20 },
+  { date: new Date(2022, 2, 10), category: 'rent', title: 'Aluguel Apt', value: 2500 },
+  { date: new Date(2022, 2, 15), category: 'salary', title: 'Salário Poster', value: 3500 }
+]
