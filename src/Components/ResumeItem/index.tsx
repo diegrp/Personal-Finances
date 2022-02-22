91e418cf74fd36497ae087f33dac782bd67f1d68
Diego Rocha <dkxever@gmail.com>
2022-02-22 17:35:48 -0300

    Passing and arranging the props for our ResumeItem

---
+++ src/Components/ResumeItem/index.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  title: string
+  value: number
+  color?: string
+}
+
+// Receitas, despesas e balanço dos valores de nossos itens
+
+export const ResumeItem = ( { title, value, color }: Props ) => {
+  return(
+    <div></div>
+  )
+}
